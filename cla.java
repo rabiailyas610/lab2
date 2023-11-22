d0f7d9e4c953573f403eb669e6b344ce6b8cec9d
rabiailyas610 <151493696+rabiailyas610@users.noreply.github.com>
2023-11-22 05:10:41 +0500

    Update cla.java

--- cla.java
+++ cla.java
@@ -7,11 +7,24 @@ public class cla
 		System.out.println("Enter two integer");
 		float a=s.nextFloat();
 		float b=s.nextFloat();
-		System.out.println("Addition :"+(a+b));
-		System.out.println("Subtraction :"+(a-b));
-		System.out.println("Multiplication :"+a*b);
-		System.out.println("Division:"+a/b);
-		System.out.println("Modulo:"+a%b);
-		
+		System.out.println("Enter an operator : ");
+		char c=s.nextAt();
+		switch(c)
+			{
+				case '+':
+					System.out.println("Addition : "+(a+b));
+					break;
+				case '-':
+					System.out.println("Subtraction : "+(a-b));
+					break;
+				case '*';
+					System.out.println("Multiplication : "+(a*b));
+					break;
+				case '/':
+					System.out.println("Division : "+(a/b));
+					break;
+				case '%':
+					System.out.println("Modulo : "+(a%b));
+			}
 	}
 }
